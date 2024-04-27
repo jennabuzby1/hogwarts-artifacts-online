0aa53f1d84a7aea0f38830ad32a45139ba5367e2
jennabuzby1 <146042997+jennabuzby1@users.noreply.github.com>
2024-04-26 22:40:45 -0500

    Implement find artifacts by search criteria with Spring Data JPA Specifications.

--- src/main/java/edu/tcu/cd/hogwartsartifactsonline/artifact/artifactRepository.java
+++ src/main/java/edu/tcu/cd/hogwartsartifactsonline/artifact/artifactRepository.java
@@ -1,10 +1,9 @@
 package edu.tcu.cd.hogwartsartifactsonline.artifact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface artifactRepository extends JpaRepository<artifact, String> {
-
+public interface artifactRepository extends JpaRepository<artifact, String>, JpaSpecificationExecutor<artifact> {
 }
